9e20b8aba223c64874a3e34f6e5ffdb48d21b09a
Tom St Denis <tom.stdenis@amd.com>
2017-05-25 09:28:32 -0400

    use pulseIn() instead of a loop...

--- echo.cpp
+++ echo.cpp
@@ -3,30 +3,20 @@
 
 unsigned ultrasonic_mm(int trig, int echo)
 {
-  unsigned long v;
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
-  while (digitalRead(echo) == LOW);
-  v = micros();
-  while (digitalRead(echo) == HIGH);
-  v = micros() - v;
-  return (v * 10) / 58;
+  return (pulseIn(echo, HIGH, 1000000) * 10) / 58;
 }
 
 unsigned ultrasonic1_mm(int pin)
 {
-  unsigned long v;
   pinMode(pin, OUTPUT);
   digitalWrite(pin, HIGH);
   delayMicroseconds(10);
   digitalWrite(pin, LOW);
   pinMode(pin, INPUT);
-  while (digitalRead(pin) == LOW);
-  v = micros();
-  while (digitalRead(pin) == HIGH);
-  v = micros() - v;
-  return (v * 10) / 58;
+  return (pulseIn(pin, HIGH, 1000000) * 10) / 58;
 }
